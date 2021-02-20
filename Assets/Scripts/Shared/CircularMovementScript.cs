fe4ec365796b0246dece2cd3b625a52f81004d07
Harry Sadler <harrysadlermusic@gmail.com>
2021-02-20 17:10:43 +0100

    more sphere object experimentation

---
+++ Assets/Scripts/Shared/CircularMovementScript.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CircularMovementScript : MonoBehaviour
+{
+
+    // Applies circular movement to a game object
+
+
+    public Transform rotationCenter;
+    public float rotationRadius = 2f;
+    public float angularSpeed = 2f;
+
+    private float posX, posZ, angle = 0f;
+
+
+    void Update()
+    {
+        posX = rotationCenter.position.x + Mathf.Cos(angle) * rotationRadius;
+        posZ = rotationCenter.position.z + Mathf.Sin(angle) * rotationRadius;
+        transform.position = new Vector3(posX, transform.position.y, posZ);
+        angle = angle + Time.deltaTime * angularSpeed;
+        if(angle >= 360f) {
+            angle = 0f;
+        }
+    }
+
+
+}
