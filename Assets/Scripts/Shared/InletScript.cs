eddd9eec525aa4497ac57a3112453bf58d806d4b
Harry Sadler <harrysadlermusic@gmail.com>
2021-02-22 09:53:57 +0100

    start work on an Inlet GameObject for purpose of mass spawning of ScienceElements

--- Assets/Scripts/Shared/InletScript.cs
+++ Assets/Scripts/Shared/InletScript.cs
@@ -14,23 +14,48 @@ public class InletScript : MonoBehaviour
     public int spawnColumnLength = 10;
     public float propulsionForce = 0f;
     public float secondsPerSpawn = 1f;
+    public int spawnPoolSize = 10000;
 
     private int spawnCount = 0;
+    private Stack<GameObject> spawnPool;
+    private Rect guiRect = new Rect(0, 0, 100, 100);
 
 
-    void Start() {}
+    void Start() {
+        this.spawnPool = new Stack<GameObject>();
+        this.FillSpawnPool();
+        Debug.Log(spawnPool.ToString());
+    }
 
     void Update() {
-        Instantiate(spawnObject, spawnPointTransform.position, Quaternion.identity);
-        spawnCount += 1;
-        Debug.Log("Spawn count: " + spawnCount.ToString());
+        GameObject go = spawnPool.Pop();
+        go.SetActive(true);
+        this.spawnCount += 1;
+        float fps = 1.0f / Time.smoothDeltaTime;
+        if(fps < 35f) {
+            Debug.Log("Spawn count: " + this.spawnCount.ToString() + ", Fps: " + fps.ToString("#.00"));
+        }
     }
 
     void OnGUI() {
+        float fps = 1.0f / Time.smoothDeltaTime;
+        string displayText = "Spawn count: " + this.spawnCount.ToString() + ", Fps: " + fps.ToString("#.00");
         GUI.Label(
-            new Rect(0, 0, 100, 100), 
-            (1.0f / Time.smoothDeltaTime).ToString("#.00")
+            this.guiRect, 
+            displayText
         );
+    }
+
+    private void FillSpawnPool() {
+        for (int i = 0; i < this.spawnPoolSize; i++) {
+            GameObject go = Instantiate(
+                this.spawnObject, 
+                this.spawnPointTransform.position, 
+                Quaternion.identity
+            ) as GameObject;
+            this.spawnPool.Push(go);
+            go.SetActive(false);
+        }
     }
 
 
