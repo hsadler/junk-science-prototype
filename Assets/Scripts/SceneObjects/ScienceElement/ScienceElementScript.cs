c415d5190c9caa2bfbdd519ae5849b77a4178dcc
Harry Sadler <harrysadlermusic@gmail.com>
2021-02-25 23:01:12 +0100

    start scene for testing science-element mixtures

---
+++ Assets/Scripts/SceneObjects/ScienceElement/ScienceElementScript.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScienceElementScript : MonoBehaviour {
+
+
+    public bool activated = false;
+
+
+    void Start() {}
+
+    void Update() {}
+
+    private void OnCollisionEnter(Collision collision) {
+        Debug.Log("Collided with gameObject.name: " + collision.gameObject.name);
+        //if(!this.activated && collision.gameObject.name)
+    }
+
+
+}
