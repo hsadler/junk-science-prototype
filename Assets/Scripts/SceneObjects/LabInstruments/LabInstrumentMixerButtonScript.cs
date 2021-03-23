32d6ad118f2428d9eae614aab80ed0009e4c6428
Harry Sadler <harrysadlermusic@gmail.com>
2021-03-23 22:03:46 +0100

    get lab-instrument-mixer on/off button working

---
+++ Assets/Scripts/SceneObjects/LabInstruments/LabInstrumentMixerButtonScript.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LabInstrumentMixerButtonScript : MonoBehaviour
+{
+
+
+    public LabInstrumentMixerScript labInstrumentMixerScript;
+    public MeshRenderer meshRenderer;
+
+
+    // UNITY HOOKS
+
+    void Start()
+    {
+        this.meshRenderer = GetComponent<MeshRenderer>();
+        this.meshRenderer.material = labInstrumentMixerScript.buttonOffMaterial;
+    }
+
+    void Update()
+    {
+        
+    }
+
+    // INTERFACE METHODS
+
+    public void PlayerLeftClickInteraction(PlayerInteractionMessage message)
+    {
+        if (gameObject == message.hit.collider.gameObject)
+        {
+            this.labInstrumentMixerScript.ToggleOnOff();
+        }
+    }
+
+
+}
---
+++ Assets/Scripts/SceneObjects/LabInstruments/LabInstrumentMixerScript.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LabInstrumentMixerScript : MonoBehaviour
+{
+
+
+    public bool isOn = false;
+
+    // on/off button
+    public LabInstrumentMixerButtonScript labInstrumentMixerButtonScript;
+    public Material buttonOnMaterial;
+    public Material buttonOffMaterial;
+
+
+    // UNITY HOOKS
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+
+    // INTERFACE METHODS
+
+    public void ToggleOnOff()
+    {
+        if (this.isOn)
+        {
+            this.isOn = false;
+            this.labInstrumentMixerButtonScript.meshRenderer.material = this.buttonOffMaterial;
+        }
+        else
+        {
+            this.isOn = true;
+            this.labInstrumentMixerButtonScript.meshRenderer.material = this.buttonOnMaterial;
+        }
+
+    }
+
+
+}
