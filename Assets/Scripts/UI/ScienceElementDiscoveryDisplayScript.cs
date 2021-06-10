1e33c3274e5abd7f610847b91b5807973337210c
Harry Sadler <harrysadlermusic@gmail.com>
2021-06-10 23:35:03 +0200

    add text mesh pro to project, stub science-element-discovery UI

---
+++ Assets/Scripts/UI/ScienceElementDiscoveryDisplayScript.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class ScienceElementDiscoveryDisplayScript : MonoBehaviour
+{
+
+
+    public GameObject textDisplayGO;
+    private TextMeshProUGUI textDisplay;
+
+
+    // UNITY HOOKS
+
+    void Start() {
+        this.textDisplay = textDisplayGO.GetComponent<TextMeshProUGUI>();
+        this.textDisplay.text = "";
+    }
+
+    void Update() {}
+
+
+}
