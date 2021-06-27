8dea585a800eeebfd5be06f9ed446147059778e1
Harry Sadler <harrysadlermusic@gmail.com>
2021-06-27 23:05:09 +0200

    add new science-element constants, add notes for science-element addition steps

--- Assets/Scripts/Constants.cs
+++ Assets/Scripts/Constants.cs
@@ -17,6 +17,33 @@ public static class Constants
     public const string SE_STEAM_TAG = "science-element-steam";
     public const string SE_STEAM_DISPLAY_NAME = "steam";
 
+    public const string SE_EARTH_TAG = "science-element-earth";
+    public const string SE_EARTH_DISPLAY_NAME = "earth";
+
+    public const string SE_MUD_TAG = "science-element-mud";
+    public const string SE_MUD_DISPLAY_NAME = "mud";
+
+    public const string SE_STONE_TAG = "science-element-stone";
+    public const string SE_STONE_DISPLAY_NAME = "stone";
+
+    public const string SE_ORE_TAG = "science-element-ore";
+    public const string SE_ORE_DISPLAY_NAME = "ore";
+
+    public const string SE_SLAG_TAG = "science-element-slag";
+    public const string SE_SLAG_DISPLAY_NAME = "slag";
+
+    public const string SE_METAL_TAG = "science-element-metal";
+    public const string SE_METAL_DISPLAY_NAME = "metal";
+
+    public const string SE_LAVA_TAG = "science-element-lava";
+    public const string SE_LAVA_DISPLAY_NAME = "lava";
+
+    public const string SE_CLAY_TAG = "science-element-clay";
+    public const string SE_CLAY_DISPLAY_NAME = "clay";
+
+    public const string SE_BRICK_TAG = "science-element-brick";
+    public const string SE_BRICK_DISPLAY_NAME = "brick";
+
 
     public const int DISCOVERY_DISPLAY_DURATION = 2;
 
